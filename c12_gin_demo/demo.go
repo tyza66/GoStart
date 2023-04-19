0e58dcb5b212d42acbef61da68def1d01ff18f40
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-04-19 15:34:21 +0800

    update

--- c12_gin_demo/demo.go
+++ c12_gin_demo/demo.go
@@ -13,16 +13,30 @@ func main() {
 
 	//加载静态界面
 	ginServer.LoadHTMLGlob("c12_gin_demo/templates/*")
+	//加载静态资源
+	ginServer.Static("/static", "./c12_gin_demo/static")
 	//访问地址 处理请求
 	ginServer.GET("/hello", func(context *gin.Context) {
 		context.JSON(200, gin.H{"msg": "giao"}) //返回json数据
 	})
-
+	//返回页面
 	ginServer.GET("/", func(context *gin.Context) {
-		context.HTML(http.StatusOK,"index.html", gin.H{
+		context.HTML(http.StatusOK, "index.html", gin.H{
 			"msg": "Hello world",
 		})
 	})
+	//获取传递的参数 get?方式
+	ginServer.GET("/get/user", func(context *gin.Context) {
+		context.Query("userId")
+        context.JSON(http.StatusOK, gin.H{"msg": "giao"}) //返回json数据
+    })
+
+    //启动服务
+    ginServer.Run(":8080")
+}
+
+	//使用路径的方式传递参数
+
 
 	ginServer.Run("localhost:8080")
 }
