70f7956ccb28f8c2facbd16554c476a6bac32c67
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-04-19 15:20:20 +0800

    update

--- c12_gin_demo/demo.go
+++ c12_gin_demo/demo.go
@@ -1,7 +1,32 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/thinkerou/favicon"
+	"net/http"
+)
 
 func main() {
+	//创建一个gin服务
+	ginServer := gin.Default()
+	ginServer.Use(favicon.New("c12_gin_demo/favicon.ico"))
 
+	//加载静态界面
+	ginServer.LoadHTMLGlob("c12_gin_demo/templates/*")
+	//访问地址 处理请求
+	ginServer.GET("/hello", func(context *gin.Context) {
+		context.JSON(200, gin.H{"msg": "giao"}) //返回json数据
+	})
+
+	ginServer.GET("/", func(context *gin.Context) {
+		context.HTML(http.StatusOK,"index.html", gin.H{
+			"msg": "Hello world",
+		})
+	})
+
+	ginServer.Run("localhost:8080")
 }
+
+/*
+	Restful Api就是做不同事情用不同的请求 gin支持
+*/
